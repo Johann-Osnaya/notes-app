2e9fcad6b1aa86c554df7f55466c437e777c064a
Johann Osnaya <johann.osnaya@gmail.com>
2024-04-15 03:25:01 -0600

    Fix vercel.json

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ let notes = [
 ]*/
 
 app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
+    response.send(__dirname + 'build/index.html')
 })
 
 app.get('/api/notes', (request, response) => {
