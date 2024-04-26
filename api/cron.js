634342d05efe3215d4fb5870e469f2940247d279
Johann Osnaya <johann.osnaya@gmail.com>
2024-04-26 01:24:39 -0600

    Fix index.html path

---
+++ api/cron.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const Note = require('../models/note')
+
+app.delete('/api/notes', (request, response, next) => {
+    Note.deleteMany()
+    .then(result => {
+        response.status(204).end()
+    })
+    .catch(error => next(error))
+})
